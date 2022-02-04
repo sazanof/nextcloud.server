452af4fdc13e5ed3591a65add51627cfe733a45b
Vincent Petry <vincent@nextcloud.com>
2022-02-04 15:36:33 +0100

    Handle OCS-APIRequest header also in app framework controller
    Fixes the auth tests that relied on the session
    
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>

--- lib/private/AppFramework/Middleware/Security/SecurityMiddleware.php
+++ lib/private/AppFramework/Middleware/Security/SecurityMiddleware.php
@@ -207,7 +207,10 @@ class SecurityMiddleware extends Middleware {
 			 * This allows oauth apps (e.g. moodle) to use the OCS endpoints
 			 */
 			if (!$this->request->passesCSRFCheck() && !(
-					$controller instanceof OCSController && (
+						(
+							$controller instanceof OCSController ||
+							$controller instanceof Controller
+						) && (
 						$this->request->getHeader('OCS-APIREQUEST') === 'true' ||
 						strpos($this->request->getHeader('Authorization'), 'Bearer ') === 0
 					)
